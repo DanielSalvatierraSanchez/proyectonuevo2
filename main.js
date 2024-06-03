8ab71d2f1a8917264cd28568728883ab6a3d3ca6
dsalvatierra <dss250583@gmail.com>
2024-06-03 02:41:10 +0200

    main

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ import './style.css'
 const app = document.querySelector('#app')
 const h1 = document.createElement('h1')
 h1.id = 'h1'
-h1.textContent = 'Hola'
+h1.textContent = 'Hola Dani'
 app.appendChild(h1)
