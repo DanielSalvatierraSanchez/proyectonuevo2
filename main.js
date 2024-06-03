31e15b27f7e881c394218a6df85250ae0218b737
dsalvatierra <dss250583@gmail.com>
2024-06-03 22:22:21 +0200

    .

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import './style.css'
 
 const app = document.querySelector('#app')
+
 const h1 = document.createElement('h1')
-h1.id = 'h1'
+const h2 = document.createElement('h1')
+const h3 = document.createElement('h1')
+
 h1.textContent = 'Hola Dani'
-app.appendChild(h1)
+h2.textContent = 'Pruebas'
+h3.textContent = 'de Git'
+
+app.append(h1, h2, h3)
